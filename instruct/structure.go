571983b5c4eb447ddf03db2fc30400e63c0c632f
Liliane Dabo <lilianedabo@outlook.com>
2023-09-29 09:43:58 +0200

    reading of file achieved. Now need the last conversion to hexadecimal.

--- instruct/structure.go
+++ instruct/structure.go
@@ -8,10 +8,10 @@ import (
 
 // Chip8 represents CPU CHIP-8 structure
 type Chip8 struct {
-	Memory     [4096]byte    // 4096 octets memory
+	memory     [4096]byte    // 4096 octets memory
 	registers  [16]byte      // 16 registers of 8 bits
 	index      uint16        // Adress register of 16 bits
-	pc         uint16        // Program counter of 16 bits
+	pc         uint16        // Program counter of 16 bits (commence a 0x200)
 	stack      [16]uint16    // Stack of 16 registers of 16 bits
 	sp         uint8         // Pointer of the stack
 	delayTimer byte          // Delay register of 8 bits
@@ -23,26 +23,31 @@ type Chip8 struct {
 
 var Cpu Chip8
 
-func ReadFileAndReturnByteArray(extractedFilePath string) ([]byte, error) {
-	file, err := os.Open("1-chip8-logo.ch8")
+// offset = setting to a custom point, 0x200 because default start for CHIP-8 <- but not that exactly but go equivalent ( 0xc8 )
+
+func TransferROMToMemory(filePath string) {
+	offset := 0xc8
+	dataFromROM, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
 
-	fileinfo, err := file.Stat()
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
+	copy(Cpu.memory[offset:], dataFromROM)
+	fmt.Println("Transfer complete.")
+	fmt.Println("Checking data :")
+
+	for i, val := range Cpu.memory {
+		sprintf := fmt.Sprintf("%v is current value, %v is current index", val, i)
+		fmt.Println(sprintf)
 	}
-
-	filesize := fileinfo.Size()
-	buffer := make([]byte, filesize)
-
-	_, err = file.Read(buffer)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	return buffer, nil
 }
+
+/*func Transferx200ToRegister() {
+	for i, val := range Cpu.memory[] {
+		while i > 200 <= 459  {
+			for y := 1; i%2 == 0; y++ {
+				Cpu.registers[y] = append(Cpu.registers[y], string(val))
+			}
+		}
+	}
+}*/
--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"CHIP8emulator/instruct"
 	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"log"
-	"time"
 )
 
 var img *ebiten.Image
@@ -16,7 +14,6 @@ type Game struct{}
 
 func init() {
 	var err error
-	img, _, err = ebitenutil.NewImageFromFile("chip-8-logo.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,43 +24,6 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	/*ebitenutil.DebugPrint(screen, "Hello, World!")*/
-	/*ebitenutil.DebugPrint(screen, "Hello World !")
-	ebitenutil.DebugPrintAt(screen, instruct.Opcode00E0(), 0, 11)*/
-
-	/*
-	* clear the screen
-
-	 */
-	/*
-	* draw sprite to screen (only aligned)
-
-	 */
-	/*
-
-		instruct.Opcode6xnn()*/
-	/*	instruct.OpcodeDxyn(screen, img)
-	 */
-	time.Sleep(2 * time.Second)
-
-	instruct.OpcodeDxyn(screen, img)
-	fmt.Println("Drew image")
-
-	time.Sleep(2 * time.Second)
-
-	for _, val := range array {
-		for i := range instruct.Cpu.Memory {
-			instruct.Cpu.Memory[i] = val
-		}
-	}
-	for i := range instruct.Cpu.Memory {
-		instruct.Opcode(instruct.Cpu.Memory[i])
-		fmt.Println("Going through memory table")
-		s := fmt.Sprintf("%d is here", instruct.Cpu.Memory[i])
-		fmt.Println(s)
-	}
-	fmt.Println("Trying to clear")
-	fmt.Println("Done with clearing")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -74,15 +34,10 @@ func main() {
 	ebiten.SetWindowSize(760, 385)
 	ebiten.SetWindowTitle("Hello, World!")
 
-	array, _ = instruct.ReadFileAndReturnByteArray("1-chip8-logo.ch8")
+	instruct.TransferROMToMemory("1-chip8-logo.ch8")
 
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
-	/*	instruct.Opcode00E0()
-		instruct.Opcode6xnn()
-		instruct.OpcodeAnnn()
-		instruct.OpcodeDxyn()
-		instruct.Opcode1nnn()*/
 	fmt.Println("Goodbye, World!")
 }
